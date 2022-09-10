b0987c21e15c9cb71c3e16b71267e6900db4af42
iboy04 <odunayoibitoye@gmail.com>
2022-09-09 21:33:40 -0700

    Commit Variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,17 +18,17 @@ int main(void)
 					n=4;
 					if (n>0)
 					{
-						printf("%d\n", n, "is positive")
+						printf("%d\n", n, "is positive");
 					}
 						else
 						{
 							if(n=0)
 							{
-						printf("%d\n", n, "is zero")
+						printf("%d\n", n, "is zero");
 							}
 							else
 							{
-							printf("%d\n", n, "is negative")
+							printf("%d\n", n, "is negative");
 							}
 						}
 					/* your code goes there */
