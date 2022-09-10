a7a19ad27469bd55456376e41e5d937308ed12c7
iboy04 <odunayoibitoye@gmail.com>
2022-09-09 21:15:21 -0700

    Negative Posiyive coommit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+/* more headers goes there */
+
+
+
+/* betty style doc for function main goes there */
+
+int main(void)
+
+{
+		int n;
+			srand(time(0));
+				n = rand() - RAND_MAX / 2;
+					n=4;
+					if (n>0)
+					{
+						printf("%d\n", n, "is positive")
+					}
+						else
+						{
+							if(n=0) 
+							{
+						printf("%d\n", n, "is zero")
+							}
+							else
+							{
+							printf("%d\n", n, "is negative")
+							}
+						}
+					/* your code goes there */
+
+					return (0);
+}
