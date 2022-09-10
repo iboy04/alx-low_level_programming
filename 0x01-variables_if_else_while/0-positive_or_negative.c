2cee4ddada3c02a797007a3aec4fdb211fb86fc7
iboy04 <odunayoibitoye@gmail.com>
2022-09-10 14:27:02 -0700

    Commit v

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,21 @@
 
 /**
 * main - Prints whether n is Positive or negative
-* Number stored in the variable n
-* Return; Always 0 (success)
+* 
+* Return: Always 0 (success)
 */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
 	}
-	
+
 	else if (n == 0)
 	{
 		printf("%d is zero\n", n);
