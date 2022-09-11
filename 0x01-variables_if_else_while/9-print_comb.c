a4479cb49918b0b907ade1a3261c0750912c49bd
iboy04 <odunayoibitoye@gmail.com>
2022-09-11 01:03:55 -0700

    Combo commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success/Correct
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchae('\n');
+	return (0);
+}
