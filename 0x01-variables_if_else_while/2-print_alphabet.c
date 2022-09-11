586b2a24999ee03a36a5808d7b7f3ee57860df3d
iboy04 <odunayoibitoye@gmail.com>
2022-09-10 23:29:07 -0700

    Alphabet commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success/correct)
+*/
+int main(void)
+{
+	char 1;
+
+	for (i = 'a';i <= '2'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	returm (0);
+}
