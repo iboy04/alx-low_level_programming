5e9dd57b68fb8c84ec096d030c1172924e9b63f1
iboy04 <odunayoibitoye@gmail.com>
2022-09-10 23:52:33 -0700

    Alphabet Upper/Lower case commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,20 @@
 /**
 * main - Entry point
 *
-* Return: Always 0 (Success/correct)
+* Return: Always 0 (Success/Correct)
 */
 int main(void)
 {
 	char i;
+	char j;
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
