b1031ee2def75eadd06b8a3ad1879db65e4db388
iboy04 <odunayoibitoye@gmail.com>
2022-09-11 00:19:51 -0700

    Numberz commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success/Correct)
+*/
+int main(void)
+{
+	char i;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
