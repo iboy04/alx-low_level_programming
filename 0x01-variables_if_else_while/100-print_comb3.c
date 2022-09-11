a27153f8046fe3f4b26d105bc63559529598a1c9
iboy04 <odunayoibitoye@gmail.com>
2022-09-11 01:14:06 -0700

    Combo Digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry Point
+*
+* Return: Always 0 (Success/Correct)
+*/
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j =48; j < 58; j++)
+		{
+			if (i != j && i < j)
+			{
+				puchar(i);
+				putchar(j);
+				if (j == 57 && i == 56)
+				{
+					break
+
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
