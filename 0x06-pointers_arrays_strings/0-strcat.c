4dcda4363bff49f263b0ec902b5e8f25f698e73f
iboy04 <odunayoibitoye@gmail.com>
2022-09-22 11:38:56 -0700

    Main h commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,18 @@ char *_strcat(char *dest, char *src)
 {
 int i, j:
 
-	i = 0;
+i = 0;
 while (dest[i] != '\0')
 {
-	i++;
+i++;
 }
 
 j = 0;
 while (src[j] != '\0')
 {
-	dest[i] = src[j];
-	j++;
-	i++;
+dest[i] = src[j];
+j++;
+i++;
 }
 dest[i] = '\0';
 
