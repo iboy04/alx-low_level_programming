abc0b5ae4571459c9f7abe88039d48d38e21d636
iboy04 <42031180+iboy04@users.noreply.github.com>
2022-09-22 19:45:03 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
-* _strcat - concatenetes two strings
-* @dest: A pointer to a character that will be changed
-* @src: A pointer to a character that will also be changed
-* Return: dest
-*/
+ *_strcat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will also be changed
+ *Return: dest
+ */
 
 char *_strcat(char *dest, char *src)
 {
-int i, j:
+int i, j;
 
 i = 0;
 while (dest[i] != '\0')
